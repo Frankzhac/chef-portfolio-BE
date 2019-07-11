fa883fad72888ab7b397ddaba162486bc576cd9d
Frankzhac <Frank.hemle@gmail.com>
2019-07-11 13:13:23 -0400

    users table built for onboarding process

---
+++ database/migrations/20190711124333_users.js
@@ -0,0 +1,43 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', user => {
+      users.increments();
+
+      user
+        .string('email', 100)
+        .notNullable()
+        .unique();
+
+      user
+        .string('name', 100)
+        .notNullable()
+        .unique();
+
+       user
+         .string('address', 100)
+         .notNullable()
+         .unique();
+
+       user
+         .string('city', 100)
+         .notNullable()
+
+       user
+         .string('state', 100)
+         .notNullable()
+         .unique();
+
+       user
+         .string('zip code', 255)
+         .notNullable()
+         .unique();
+         
+       user
+         .string('password', 255)
+         .notNullable()
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
