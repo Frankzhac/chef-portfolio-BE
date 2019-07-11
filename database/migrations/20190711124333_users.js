83a3809a3947b292d74eeac0876ae27f0db6e250
Frankzhac <Frank.hemle@gmail.com>
2019-07-11 13:51:35 -0400

    dbConfig and db built

--- database/migrations/20190711124333_users.js
+++ database/migrations/20190711124333_users.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', user => {
-      users.increments();
+      user.increments();
 
       user
         .string('email', 100)
